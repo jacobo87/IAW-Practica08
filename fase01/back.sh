de8e994b853dbb0d139688858d463cf760b4670f
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2020-11-26 11:15:47 +0100

    Update

--- fase01/back.sh
+++ fase01/back.sh
@@ -18,7 +18,7 @@ apt update
 # Instalamos el sistema gestor de base de datos
 apt install mysql-server -y
 # Editamos el archivo de configuración de MySQL, modificando la línea 
-sed -i 's/127.0.0.1/$IPWEB/' /etc/mysql/mysql.conf.d/mysqld.cnf 
+sed -i "s/127.0.0.1/$IPWEB/" /etc/mysql/mysql.conf.d/mysqld.cnf 
 # Reiniciamos el servicio
 sudo /etc/init.d/mysql restart
 # Actualizamos la contraseña de root de MySQL
