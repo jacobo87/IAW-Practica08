608acda55a16e2d64f22d108a0b927855dccbd84
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2020-11-30 13:13:08 +0100

    Update database.sql

--- fase00/database.sql
+++ fase00/database.sql
@@ -2,6 +2,6 @@ DROP DATABASE IF EXISTS wp_db;
 CREATE DATABASE wp_db CHARSET utf8mb4;
 USE wp_db;
 
-CREATE USER IF NOT EXISTS 'wp_user'@'%';
-SET PASSWORD FOR 'wp_user'@'%' = 'wp_pass';
+CREATE USER IF NOT EXISTS 'wp_user'@'localhost';
+SET PASSWORD FOR 'wp_user'@'localhost' = 'wp_pass';
 GRANT ALL PRIVILEGES ON wp_db.* TO 'wp_user'@'%';
