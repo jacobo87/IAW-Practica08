b72eeb2f21f98aa7b898d1c88c5836646b57fb3c
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2020-12-01 13:08:12 +0100

    Update front.sh

--- fase01/front.sh
+++ fase01/front.sh
@@ -36,13 +36,13 @@ sed -i "s/localhost/$IPMYSQL/" /home/ubuntu/wp-config.php
 # Modificamos Unique Keys del archivo de configuracion de WP
 # Borramos 
 sed -i "/AUTH_KEY/d" /var/www/html/wordpress/wp-config.php       
-sed -i "SECURE_AUTH_KEY/d" /var/www/html/wordpress/wp-config.php
-sed -i "LOGGED_IN_KEY/d" /var/www/html/wordpress/wp-config.php   
-sed -i "NONCE_KEY/d" /var/www/html/wordpress/wp-config.php       
-sed -i "AUTH_SALT/d" /var/www/html/wordpress/wp-config.php       
-sed -i "SECURE_AUTH_SALT/d" /var/www/html/wordpress/wp-config.php
-sed -i "LOGGED_IN_SALT/d" /var/www/html/wordpress/wp-config.php  
-sed -i "NONCE_SALT/d" /var/www/html/wordpress/wp-config.php
+sed -i "/SECURE_AUTH_KEY/d" /var/www/html/wordpress/wp-config.php
+sed -i "/LOGGED_IN_KEY/d" /var/www/html/wordpress/wp-config.php   
+sed -i "/NONCE_KEY/d" /var/www/html/wordpress/wp-config.php       
+sed -i "/AUTH_SALT/d" /var/www/html/wordpress/wp-config.php       
+sed -i "/SECURE_AUTH_SALT/d" /var/www/html/wordpress/wp-config.php
+sed -i "/LOGGED_IN_SALT/d" /var/www/html/wordpress/wp-config.php  
+sed -i "/NONCE_SALT/d" /var/www/html/wordpress/wp-config.php
 # Creamos la variable con la salida de la api
 SECURITY_KEYS=$(https://api.wordpress.org/secret-key/1.1/salt/)
 # Reemplaza el caracter / por el _
